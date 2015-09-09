f3e6c354b399bc991b123b030f2257e78ed85919
daniel <dkushner@csumb.edu>
2015-09-09 15:47:03 -0700

    no message

--- Assignment2.java
+++ Assignment2.java
@@ -3,6 +3,7 @@ package cst338;
  * Here is the base for our first team assignment in cst 338
  * This is a test to ensure github and sourceTree are synced 
  * test two does this update correctly? 
+ * Yes it is pushing correctly
  */
 public class Assignment2 {
 
