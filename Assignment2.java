78e75ee51cac3303d2fc71c13d01e638600f20b5
daniel <dkushner@csumb.edu>
2015-09-09 15:45:03 -0700

    no message

---
+++ Assignment2.java
@@ -0,0 +1,13 @@
+package cst338;
+/*
+ * Here is the base for our first team assignment in cst 338
+ * This is a test to ensure github and sourceTree are synced 
+ */
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
