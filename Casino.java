3114ca03fa68d70c12c9c53edd639e0cc4b533a5
csumbstudent <Summer2016>
2015-09-15 19:34:08 -0700

    Casino class
    All of the functionality of the Casino class has been implemented. It
    just needs more comments.

--- Casino.java
+++ Casino.java
@@ -1,38 +1,96 @@
-import java.util.Scanner;
-import java.lang.Math;
+/*Assignment 2, Casino
+
+  Team:
+  Christopher Rendall
+  Caroline Lancaster
+  Daniel Kushner
+
+  Description:
+  This Casino application promps the user for a bet, randomly generates strings to represent
+  slot machine reels, and checks the multiplier associated with the outcome. It then displays
+  whether the user won or lost. If the user enters a bet of 0, then the game ends and displays
+  each of the user's winnings, as well as the total amount of money that they won.
+
+ */
+import java.util.Scanner; //For reading keyboard input.
+import java.lang.Math; //For random.
+
+//The Casino class is the main class for this application.
 public class Casino {
+    //Program entry point main():
     public static void main(String[] args){
-        int bet = 1;
-        TripleString thePull;
+        int bet = 0; //Initialize bet.
+        //Initialize the TripleString object.
+        TripleString thePull = new TripleString();
+        //Initialize a variable to  hold the winngings from each iteration.
         int winnings = 0;
+
+        //This is the main game loop. It runs while true, and breaks if the user enters a bet of 0.
         while(true) {
-            bet = getBet();
+            bet = getBet(); //Read a valid bet in from the user.
+            //End the loop if the user enters 0.
             if(bet == 0)
                 break;
-            thePull = pull();
+            thePull = pull(); //Get a random pull.
+            //Calculate the winnings based on the pull.
             winnings = bet * getPayMultiplier(thePull);
-            display(thePull, winnings);
+            TripleString.saveWinnings(winnings); //Save the winnings in the static member of TripleString
+            display(thePull, winnings); //Display whether the user won or lost this round.
         }
+        //Print some information for the user once they decide to quit playing.
+        System.out.println("Thank you for playing the casino!");
+        thePull.displayWinnings();
     }
+
+    /*  int getBet()
+        In: Nothing
+        Out: A valid integer bet value input by the user.
+        Description: A static function to retrieve a bet from the user. This uses keyborad input to get the bet.
+        It then checks to make sure the bet is valid, and will not return an integer until a valid bet is entered.
+     */
     static int getBet(){
         Scanner keyboardIn = new Scanner(System.in);
-        int bet = 0;
+        int bet = -1; //Set bet to any non-zero value.
         do{
+            //This loop will run until a valid bet is entered.
+            //Prompt the user for a bet.
             System.out.println("Enter a bet between 1 and 100. Enter 0 to quit.\nBet: ");
-            bet = keyboardIn.nextInt();
-            if(bet < 0 || bet > 100)
+            if(keyboardIn.hasNextInt()) //Wait for the user to enter a bet, and then check if the user has entered an integer.
+                bet = keyboardIn.nextInt(); //If they have entered an integer, then set bet to that integer.
+            else{
+                //If the user did not enter an integer, then clear the input buffer.
+                keyboardIn.next(); //Clear the input buffer.
+                bet = -1; //Set bet to an integer value outside of the allowed range.
+                continue; //Start at the beginning of this loop.
+            }
+            //If the loop makes it this far, the user has entered an integer. Check to see if it is within the allowable bet range.
+            if (bet < 0 || bet > 100)
                 System.out.println("Your bet is outside of the allowable range.");
-        } while(bet < 0 || bet > 100);
-        return bet;
+        } while(bet < 0 || bet > 100); //Continue until a valid bet is entered.
+        return bet; //Return the valid bet to the caller.
     }
+
+    /*  void display(TripleString, int)
+        In:
+        1. A TripleString object that contains information about a pull
+        2. The amount won by the user on the pull
+        Out: Nothing
+        Description: A static function to display the user's pull results and the amount of money that they won (if they won).
+    */
     public static void display(TripleString thePull, int winnings){
         System.out.println("You pulled....");
         System.out.println(thePull.toString());
         if(winnings > 0)
-            System.out.println("You won $" + winnings + "!!!");
+            System.out.println("You won $" + winnings + "!!!\n");
         else
-            System.out.println("You lost... Better luck next time...");
+            System.out.println("You lost... Better luck next time...\n");
     }
+    /*  TripleString pull()
+        In: Nothing
+        Out: A TripleString object containing three of the strings that can appear on a reel.
+        Description: This function creates a new TripleString object and sets its string members
+        randomly.
+     */
     public static TripleString pull(){
         TripleString triple = new TripleString();
         triple.setString1(randString());
@@ -73,12 +131,10 @@ public class Casino {
 class TripleString{
     private String string1, string2, string3;
     public static final short MAX_LEN = 20, MAX_PULLS=40;
-    private static int numPulls;
-    private static int[] pullWinnings;
+    private static int numPulls = 0;
+    private static int[] pullWinnings = new int[MAX_PULLS];
     public TripleString(){
         this.string1 = this.string2 = this.string3 = "";
-        this.pullWinnings = new int[MAX_PULLS];
-        this.numPulls = 0;
     }
     private boolean validString(String str){
         if(str != null && str.length() <= MAX_LEN)
@@ -89,9 +145,9 @@ class TripleString{
     public String toString(){
         return this.string1 + "    " + this.string2 + "    " + this.string3;
     }
-    public void saveWinnings(int winnings){
-        this.pullWinnings[this.numPulls] = winnings;
-        this.numPulls++;
+    public static void saveWinnings(int winnings){
+        TripleString.pullWinnings[TripleString.numPulls] = winnings;
+        TripleString.numPulls++;
     }
     public boolean setString1(String str){
         if(!this.validString(str))
@@ -121,7 +177,12 @@ class TripleString{
         return this.string3;
     }
     public void displayWinnings(){
-        for(int i = 0; i < numPulls; i++)
-            System.out.println("Pull " + (i + 1) + " : " + this.pullWinnings[i]);
+        System.out.println("Your inidividual winnings were:");
+        int totalWinnings = 0;
+        for(int i = 0; i < TripleString.numPulls; i++) {
+            System.out.print(TripleString.pullWinnings[i] + " ");
+            totalWinnings += TripleString.pullWinnings[i];
+        }
+        System.out.println("\nYour total winnings were: $" + totalWinnings);
     }
 }
